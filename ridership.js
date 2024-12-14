4b303be28a698b2c90ce1057a684b7d2f8741d1c
sushmitakhan05 <sushmitakhan05@gmail.com>
2024-12-13 20:33:00 -0500

    Add files via upload

---
+++ ridership.js
@@ -0,0 +1,155 @@
+var r = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Times Square",
+        "ridership": 29507558
+      },
+      "geometry": {
+        "coordinates": [
+          -73.98583926625733,
+          40.75558533463641
+        ],
+        "type": "Point"
+      },
+      "id": 0
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Herald Square",
+        "ridership": 15037793
+      },
+      "geometry": {
+        "coordinates": [
+          -73.98792190010701,
+          40.749444490331
+        ],
+        "type": "Point"
+      },
+      "id": 1
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Grand Central",
+        "ridership": 4002142
+      },
+      "geometry": {
+        "coordinates": [
+          -73.97681778412695,
+          40.751760940855206
+        ],
+        "type": "Point"
+      },
+      "id": 2
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Union Square",
+        "ridership": 13165975
+      },
+      "geometry": {
+        "coordinates": [
+          -73.98990834875792,
+          40.734452462356444
+        ],
+        "type": "Point"
+      },
+      "id": 3
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Penn Station 1,2,3",
+        "ridership": 8935671
+      },
+      "geometry": {
+        "coordinates": [
+          -73.99064501481651,
+          40.75100577124843
+        ],
+        "type": "Point"
+      },
+      "id": 4
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Flushing Main Street",
+        "ridership": 9206396
+      },
+      "geometry": {
+        "coordinates": [
+          -73.82995380954004,
+          40.75963780972802
+        ],
+        "type": "Point"
+      },
+      "id": 5
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Jackson Heights",
+        "ridership": 9437073
+      },
+      "geometry": {
+        "coordinates": [
+          -73.8909743415704,
+          40.746564093018975
+        ],
+        "type": "Point"
+      },
+      "id": 6
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Colombus Circle",
+        "ridership": 9310678
+      },
+      "geometry": {
+        "coordinates": [
+          -73.98167110706196,
+          40.76824815969235
+        ],
+        "type": "Point"
+      },
+      "id": 7
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Penn Station A,C,E",
+        "ridership": 9855288
+      },
+      "geometry": {
+        "coordinates": [
+          -73.99342682752363,
+          40.752178922115405
+        ],
+        "type": "Point"
+      },
+      "id": 8
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Fulton Street",
+        "ridership": 9728874
+      },
+      "geometry": {
+        "coordinates": [
+          -74.00763431083605,
+          40.710331082218175
+        ],
+        "type": "Point"
+      },
+      "id": 9
+    }
+  ]
+}
